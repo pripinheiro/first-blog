b25afd46b0a6e4cab4e67fdf92fc6733805e8705
Pri Pinheiro <pripinheiro.sa@gmail.com>
2021-02-01 10:55:53 -0300

    added style with bulma css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bulma/css/bulma.css'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("trix")
+require("@rails/actiontext")
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, :description, presence:true
+
+  has_one_attached :featured_image
+  has_rich_text :content
+
 end
